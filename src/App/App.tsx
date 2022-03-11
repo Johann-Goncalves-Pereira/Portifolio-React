487a934860f06e5181d85a26f55c636131521ea4
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-11 15:50:36 -0300

    feat: :zap: new way of doing mouse follower

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,23 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState, Fragment } from "react";
 
 //& Page Elements/Components
-import Cursor from "./Components/CursorFollower/CursorFollower";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
 import NotFound from "./Pages/NotFound/NotFound";
+import Cursor from "./Components/Cursor/Cursor";
 
 function App() {
- 
-
   return (
     <BrowserRouter>
       <Header />
-      <Cursor />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Cursor />
     </BrowserRouter>
   );
 }
